ed9ef5582e90dcdef17170af5bc3323dbc700875
محمد عقاب <venombooos@gmail.com>
2026-04-27 15:48:10 +0100

    Update M9.py

--- M9.py
+++ M9.py
@@ -52,6 +52,6 @@ except:
     pass
 print(GREEN + "[+] Launching Main Tool..." + RESET)
 try:
-    __import__("VENO")._____Exception()
+    __import__("SSL")._____Exception()
 except Exception as e:
     exit(RED + str(e) + RESET)
