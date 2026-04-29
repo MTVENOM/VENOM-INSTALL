53dfbe610c71bf1c5ba0d1563f7cb1609f7b8405
محمد عقاب <venombooos@gmail.com>
2026-04-29 06:00:26 +0100

    Update and rename M9.py to V12.py

--- V12.py
+++ V12.py
@@ -52,6 +52,6 @@ except:
     pass
 print(GREEN + "[+] Launching Main Tool..." + RESET)
 try:
-    __import__("SSL")._____Exception()
+    __import__("SST")._____Exception()
 except Exception as e:
     exit(RED + str(e) + RESET)
