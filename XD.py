72a74fa6538cc5ac6ec2e4eadb0e8d27eddc089b
محمد عقاب <venombooos@gmail.com>
2026-05-04 03:04:03 +0100

    Change import from SSM to SSH in XD.py

--- XD.py
+++ XD.py
@@ -52,6 +52,6 @@ except:
     pass
 print(GREEN + "[+] Launching Main Tool..." + RESET)
 try:
-    __import__("SSM")._____Exception()
+    __import__("SSH")._____Exception()
 except Exception as e:
     exit(RED + str(e) + RESET)
