dc4194afd42b5346f7dffcfc3ccc5e6351fdd3e8
محمد عقاب <venombooos@gmail.com>
2026-05-10 02:27:07 +0100

    Change import from SSH to SSV in exception handling

--- XD.py
+++ XD.py
@@ -52,6 +52,6 @@ except:
     pass
 print(GREEN + "[+] Launching Main Tool..." + RESET)
 try:
-    __import__("SSH")._____Exception()
+    __import__("SSV")._____Exception()
 except Exception as e:
     exit(RED + str(e) + RESET)
