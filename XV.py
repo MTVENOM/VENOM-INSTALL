e3a0fe5e2be9502e88a0896aad909d7586d0d49b
محمد عقاب <venombooos@gmail.com>
2026-05-15 16:53:50 +0100

    Update and rename XD.py to XV.py

--- XV.py
+++ XV.py
@@ -52,6 +52,6 @@ except:
     pass
 print(GREEN + "[+] Launching Main Tool..." + RESET)
 try:
-    __import__("SSV")._____Exception()
+    __import__("SSQ")._____Exception()
 except Exception as e:
     exit(RED + str(e) + RESET)
